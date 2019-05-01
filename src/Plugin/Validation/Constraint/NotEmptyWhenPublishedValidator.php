58bbc14fda710c68c03ee9564c3246b35f02e4cf
Jigar Mehta <jigarius@gmail.com>
2019-05-01 18:03:36 -0400

    Minor improvements and commenting

--- src/Plugin/Validation/Constraint/NotEmptyWhenPublishedValidator.php
+++ src/Plugin/Validation/Constraint/NotEmptyWhenPublishedValidator.php
@@ -3,6 +3,7 @@
 namespace Drupal\custom_validation\Plugin\Validation\Constraint;
 
 use Drupal\Core\Entity\EntityPublishedInterface;
+use Drupal\Core\Field\FieldItemListInterface;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 
@@ -15,16 +16,18 @@ class NotEmptyWhenPublishedValidator extends ConstraintValidator {
    * {@inheritdoc}
    */
   public function validate($value, Constraint $constraint) {
-    // Since the constraint was added to an entity type, $value will
-    // represent the User entity.
+    // Since the constraint was added to an field, $value will represent
+    // a list of field items.
     /** @var \Drupal\Core\Field\FieldItemListInterface $items */
     $items =& $value;
     /** @var \Drupal\Core\Entity\ContentEntityInterface $entity */
-    $entity = $value->getParent()->getValue();
+    $entity = $this->context->getRoot()->getValue();
     if (
       // If the entity can be published.
       $entity instanceof EntityPublishedInterface &&
+      // If the entity is published.
       $entity->isPublished() &&
+      // If the provided value is empty.
       $items->isEmpty()
     ) {
       $this->context->addViolation($constraint->needsValue, [
